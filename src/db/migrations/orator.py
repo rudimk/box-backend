96a6b2e58c6f7558a2510029ac1ee7f1fac1802d
Rudraksh MK <rudimk.cloud@gmail.com>
2019-11-02 12:18:37 +0530

    Added a folder for tracking database migrations.

---
+++ src/db/migrations/orator.py
@@ -0,0 +1,11 @@
+import os
+
+DATABASES = {
+    'mysql': {
+        'driver': 'mysql',
+        'host': os.getenv('DB_HOST'),
+        'database': os.getenv('DB_NAME'),
+        'user': os.getenv('DB_USER'),
+        'password': os.getenv('DB_PASSWD')
+    }
+}
