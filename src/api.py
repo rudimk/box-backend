4cac52cfafc3eb7bb50852844dd437e72d28b396
Rudraksh MK <rudimk.cloud@gmail.com>
2019-11-04 14:20:48 +0530

    Activated CORS on all endpoints, and opened up support for OPTIONS requests.

--- src/api.py
+++ src/api.py
@@ -1,9 +1,10 @@
 from sanic import Sanic
 from sanic.response import json
+from sanic_cors import CORS
 from db.config import db
 
 app = Sanic()
-
+CORS(app, automatic_options=True)
 
 ## Define endpoints for manipulating credentials.
 
@@ -11,37 +12,39 @@ app = Sanic()
 @app.get('/credentials')
 async def getCredentials(request):
 	credentials = db.table('credentials').get()
-	return json(credentials)
+	return json({'data': credentials, 'total': len(credentials)})
 
 
 # GET /credentials/<credential_id>
 @app.get('/credentials/<credential_id>')
 async def getCredentialByID(request, credential_id):
 	credential = db.table('credentials').where('id', int(credential_id)).first()
-	return json(credential)
+	return json({'data': credential})
 
 
 # POST /credentials
 @app.post('/credentials')
 async def createCredentials(request):
 	credentials = request.json
-	newCredentials = db.table('credentials').insert_get_id(credentials)
-	return json({'success': True, 'id': newCredentials})
+	newCredentialID = db.table('credentials').insert_get_id(credentials)
+	newCredential = db.table('credentials').where('id', newCredentialID).first()
+	return json({'data': newCredential})
 
 
 # PUT /credentials/<credential_id>
 @app.put('/credentials/<credential_id>')
 async def modifyCredentialbyID(request, credential_id):
 	updateBody = request.json
-	updatedCredentials = db.table('credentials').where('id', int(credential_id)).update(updateBody)
-	return json({'success': True, 'id': int(credential_id)})
+	updatedCredentialStatus = db.table('credentials').where('id', int(credential_id)).update(updateBody)
+	updatedCredential = db.table('credentials').where('id', int(credential_id)).first()
+	return json({'data': updatedCredential})
 
 
 # DELETE /credentials/<credential_id>
 @app.delete('/credentials/<credential_id>')
 async def deleteCredentialsByID(request, credential_id):
 	deleteCredentials = db.table('credentials').where('id', int(credential_id)).delete()
-	return json({'success': True})
+	return json({'data': None})
 
 
 if __name__ == '__main__':
